edc7b96dd4b61553fdcc5ae7da4cb0569b397317
mustafa inaya <moose.inaya@icloud.com>
2018-12-10 15:17:07 -0800

    UPLOAD, Uploaded Back Ground Img For The Main Display.

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					{/* <img src={logo} className="App-logo" alt="logo" /> */}
+					<img
+						src="https://vignette.wikia.nocookie.net/lotr/images/1/18/310993_121176884650130_120725101361975_85832_1441609629_n.jpg/revision/latest?cb=20151105155925"
+						className="back-img"
+						alt="prop"
+					/>
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">To get started, edit and save to reload.okay I think it is working</p>
+			</div>
+		);
+	}
 }
+// const Messege = (props) => {
+// 	retrun(
+// 		<div className="card">
+// 			<button onClick={props.mustafa}>>></button>
+// 		</div>
+// 	);
+// };
 
 export default App;
